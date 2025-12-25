31b312dfc98a1d4f57b5d3f8e38794b7df7b0549
smshiraz <syedmustafa205@gmail.com>
2025-12-25 18:27:08 +0530

    Correction

--- src/com/syed/corejava/StockMaxProfit.java
+++ src/com/syed/corejava/StockMaxProfit.java
@@ -9,51 +9,26 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
  */
 public class StockMaxProfit {
 
-    public static void main(String[] args) {
-        int[] prices = { 7, 1, 5, 3, 6, 4 };
-        int maxProfit = maxProfitWithNestedLoops(prices);
-        System.out.println("Maximum Profit: " + maxProfit);
-    }
+	public static void main(String[] args) {
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+		int maxProfit = maxProfit(prices);
+		System.out.println("Maximum Profit: " + maxProfit);
+	}
 
-    public static int maxProfitWithNestedLoops(int[] prices) {
-        if (prices == null || prices.length == 0)
-            return 0;
+	
 
-        int maxProfit = 0;
+	public static int maxProfit(int[] prices) {
+		int maxProfit = 0;
+		int min = prices[0];
 
-        for (int i = 0; i < prices.length - 1; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                int profit = prices[j] - prices[i];
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
-            }
-        }
-        return maxProfit;
-    }
+		for (int i = 1; i < prices.length; i++) {
+			int profit = prices[i] - min;
+			if (profit > maxProfit) {
+				maxProfit = profit;
+			}
+			min = Math.min(min, prices[i]);
+		}
 
-    // Original efficient solution for comparison
-   /* public static int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0)
-            return 0;
-
-        int minPrice = prices[0];
-        int maxProfit = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            // Update minPrice if current price is lower
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } else {
-                // Calculate profit if sold today
-                int profit = prices[i] - minPrice;
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
-            }
-        }
-        return maxProfit;
-    }
-}*/
-
+		return maxProfit;
+	}
 }
